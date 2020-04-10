b1a151cf483b6453759815299e8c7fe8d97bc927
ryan10145 <chang.ryan10145@gmail.com>
2020-04-10 15:42:56 -0400

    Add Basic Slider and P5 Code

--- public/js/p5/main.js
+++ public/js/p5/main.js
@@ -1,7 +1,30 @@
+const widthScaling = 0.9;
+const heightScaling = 0.6;
+
+let canvas;
+let testSlider;
+
+function styleCanvas() {
+    canvas.style('display', 'block');
+    canvas.style('margin', '10px');
+    let x = (windowWidth - width) / 2;
+    canvas.position(x);
+}
+
 function setup() {
-    createCanvas(640, 480);
+    canvas = createCanvas(windowWidth * widthScaling, windowHeight * heightScaling);
+    styleCanvas();
+
+    testSlider = select('#test-slider');
 }
 
 function draw() {
-    rect(50, 50, 80, 80);
+    background(255);
+    rectMode(CENTER);
+    rect(width / 2, height / 2, testSlider.value() / 100.0 * width, testSlider.value() / 100.0 * height);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth * widthScaling, windowHeight * heightScaling);
+    styleCanvas();
 }
