e0388394ea42df1f33126eeed2d62ee1eb2551f5
ryan10145 <chang.ryan10145@gmail.com>
2020-04-12 21:31:18 -0400

    Improve Mobile Dragging

--- public/js/p5/bundle.js
+++ public/js/p5/bundle.js
@@ -56,7 +56,7 @@ var currentSketch = new p5(function(sketch) {
     let mouseState = MouseState.DEFAULT;
     let mouseClickVector = null;
     let activePoint = -1;
-    let deletingPoints = false;
+    let lingeringMouse = false;
     
     sketch.setup = function() {
         canvas = sketch.createCanvas(sketch.windowWidth * widthScaling, sketch.windowHeight * heightScaling);
@@ -68,6 +68,10 @@ var currentSketch = new p5(function(sketch) {
         deleteAllPointsButton = sketch.select('#delete-all-points-button');
         deleteAllPointsButton.mousePressed(deleteAllPoints);
     }
+
+    deleteAllPoints = function() {
+        userPoints = [];
+    }
     
     sketch.draw = function() {
         update();
@@ -75,18 +79,7 @@ var currentSketch = new p5(function(sketch) {
     }
 
     update = function() {
-        if(mouseState != MouseState.DRAGGING) {
-            let closestDist = userWaypointSizeSlider.value() * userWaypointSizeSlider.value();
-            activePoint = -1;
-            mouseVector = new Vector(conv.cx(sketch.mouseX, sketch.width), conv.cx(sketch.mouseY, sketch.height));
-            for(pointIndex in userPoints) {
-                dist = userPoints[pointIndex].getDistanceToSq(mouseVector);
-                if(dist < closestDist) {
-                    activePoint = pointIndex;
-                    closestDist = dist;
-                }
-            }
-        }
+        if(!lingeringMouse) calculateActivePoint();
 
         if(mouseState == MouseState.DRAGGING) {
             if(activePoint != -1 && mouseClickVector != null) {
@@ -99,6 +92,23 @@ var currentSketch = new p5(function(sketch) {
                 mouseClickVector = new Vector(conv.cx(sketch.mouseX, sketch.width), conv.cy(sketch.mouseY, sketch.height));
             }
             else mouseState = MouseStates.DEFAULT;
+        }
+
+        console.log(mouseState);
+    }
+
+    calculateActivePoint = function() {
+        if(mouseState != MouseState.DRAGGING) {
+            let closestDist = userWaypointSizeSlider.value() * userWaypointSizeSlider.value();
+            activePoint = -1;
+            mouseVector = new Vector(conv.cx(sketch.mouseX, sketch.width), conv.cx(sketch.mouseY, sketch.height));
+            for(pointIndex in userPoints) {
+                dist = userPoints[pointIndex].getDistanceToSq(mouseVector);
+                if(dist < closestDist) {
+                    activePoint = pointIndex;
+                    closestDist = dist;
+                }
+            }
         }
     }
 
@@ -126,9 +136,10 @@ var currentSketch = new p5(function(sketch) {
     }
 
     sketch.mousePressed = function() {
+        calculateActivePoint();
+
         // ensure the mouse is within the sketch window doing anything
-        if(sketch.mouseX >= 0 && sketch.mouseX <= sketch.width && 
-            sketch.mouseY >= 0 && sketch.mouseY <= sketch.height) {
+        if(mouseInSketch()) {
 
             if(deletePointsCheckbox.elt.checked) {
                 // delete the current point
@@ -168,6 +179,20 @@ var currentSketch = new p5(function(sketch) {
         
     }
 
+    sketch.touchStarted = function() {
+        lingeringMouse = false;
+        sketch.mousePressed();
+    }
+
+    sketch.touchMoved = function() {
+        if(mouseInSketch()) return false;
+    }
+
+    sketch.touchEnded = function() {
+        lingeringMouse = true;
+        sketch.mouseReleased();
+    }
+
     sketch.keyPressed = function() {
         keyCodes.push(sketch.keyCode);
         keys.push(sketch.key);
@@ -186,8 +211,9 @@ var currentSketch = new p5(function(sketch) {
         }
     }
 
-    deleteAllPoints = function() {
-        userPoints = [];
+    mouseInSketch = function() {
+        return sketch.mouseX >= 0 && sketch.mouseX <= sketch.width && 
+            sketch.mouseY >= 0 && sketch.mouseY <= sketch.height;
     }
 })
 
