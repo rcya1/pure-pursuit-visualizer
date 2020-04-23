b695a3962d2f8d080feabfb0593ee389ab66c729
ryan10145 <chang.ryan10145@gmail.com>
2020-04-23 00:32:27 -0400

    Add All Pure Pursuit Following Code (Absolutely Not Tested At All)

---
+++ public/js/p5/follower_util.js
@@ -0,0 +1,140 @@
+// returns the index of the closest point to the given vector
+// uses the last found point to optimize the search
+getClosestPointIndex = function(points, pos, lastPointIndex = 0) {
+    let index = -1;
+    let closestDist = -1;
+
+    for(let i = lastPointIndex; i < points.length; i++) {
+        waypoint = points[i];
+        let checkDist = waypoint.getDistanceToSq(pos.getX(), pos.getY());
+
+        if(index == -1 || checkDist <= closestDist) {
+            index = i;
+            closestDist = checkDist;
+        }
+    }
+
+    return index;
+}
+
+let LookAheadResult = class {
+    constructor(t, i, lookAheadPoint) {
+        this.t = t;
+        this.i = i;
+        this.lookAheadPoint = lookAheadPoint;
+    }
+}
+
+getLookAheadPoint = function(points, pos, lookAheadDist, lastT = 0, lastIndex = 0) {
+    for(let i = lastIndex; i < points.length - 1; i++) {
+        let a = points[i];
+        let b = points[i + 1];
+
+        let t = getLookAheadPointT(pos, a.getPosition(), b.getPosition(), lookAheadDist);
+
+        // if the segment is further along or the fractional index is greater, then this is the correct point
+        if(t != -1 && i > lastIndex || t > lastT) {
+            return generateLookAheadResult(a, b, t, i);
+        }
+    }
+
+    // if no point is found, just return the last look ahead result
+    return generateLookAheadResult(points[lastIndex], points[lastIndex + 1], lastT, lastIndex);
+}
+
+generateLookAheadResult = function(a, b, t, i) {
+    let d = b.getPosition().sub(a.getPosition());
+    return new LookAheadResult(t, i, a.getPosition().add(d.mult(lastT)));
+}
+
+getLookAheadPointT = function(pos, start, end, lookAheadDist) {
+    let d = end.sub(start);
+    let f = start.sub(pos);
+
+    let a = d.dot(d);
+    let b = 2 * f.dot(d);
+    let c = f.dot(f) - lookAheadDist * lookAheadDist;
+
+    let disc = b * b - 4 * a * c;
+
+    if(disc < 0) {
+        return -1;
+    }
+    
+    disc = Math.sqrt(disc);
+    let t1 = (-b - disc) / (2 * a);
+    let t2 = (-b + disc) / (2 * a);
+
+    if(t1 >= 0 && t1 <= 1) return t1;
+    if(t2 >= 0 && t2 <= 1) return t2;
+
+    return -1;
+}
+
+getCurvatureToPoint = function(pos, angle, lookAhead) {
+    let a = -Math.tan(angle);
+    let b = 1.0;
+    let c = Math.tan(angle) * pos.getX() - pos.getY();
+
+    let x = Math.abs(a * lookAhead.getX() + b * lookAhead.getY() + c) / Math.sqrt(a * a + b * b);
+    let l = pos.getDistanceToSq(lookAhead);
+    let curvature = 2 * x / l;
+
+    let otherPoint = pos.add(new Vector(Math.cos(angle), Math.sin(angle)));
+    let side = Math.sign((otherPoints.getY() - pos.getY()) * (lookAhead.getX() - pos.getX()) - 
+        (otherPoints.getX() - pos.getX()) * (lookAhead.getY() - robot.getY()));
+
+    return curvature * side;
+}
+
+PurePursuitFollower = class {
+    lastT = 0.0;
+    lastLookAheadIndex = 0;
+    lastClosestIndex = 0;
+    leftSpeed = 0;
+    rightSpeed = 0;
+    lastTime = -1;
+
+    constructor(lookAheadDist, driveWidth, maxAcceleration) {
+        this.lookAheadDist = lookAheadDist;
+        this.driveWidth = driveWidth;
+        this.maxAcceleration = maxAcceleration;
+    }
+}
+
+followPath = function(robot, follower, points, currentTime) {
+    if(points.length == 0) return;
+
+    let lookAheadResult = getLookAheadPoint(points, robot.getPosition(), follower.lookAheadDist, 
+        follower.lastT, follower.lastIndex);
+    follower.lastT = lookAheadResult.t;
+    follower.lastLookAheadIndex = lookAheadResult.i;
+    let lookAheadPoint = lookAheadResult.lookAheadPoint;
+
+    let curvature = getCurvatureToPoint(robot.getPosition(), robot.getAngle(), lookAheadPoint);
+    follower.lastClosestIndex = getClosestPointIndex(points, robot.getPosition(), follower.lastClosestIndex);
+    let targetVelocity = points[follower.lastClosestIndex].getTargetVelocity();
+
+    let tempLeft = targetVelocity * (2.0 + curvature * follower.driveWidth) / 2.0;
+    let tempRight = targetVelocity * (2.0 - curvature * DRIVE_WIDTH) / 2.0;
+
+    if(follower.lastCall == -1) lastCall = currentTime;
+    let maxChange = (currentTime - lastCall) / 1000.0 * follower.maxAcceleration;
+    follower.leftSpeed += constrain(tempLeft - follower.leftSpeed, maxChange, -maxChange);
+    follower.rightSpeed += constrain(tempRight - follower.rightSpeed, maxChange, -maxChange);
+
+    robot.setLeft(follower.leftSpeed);
+    robot.setRight(follower.rightSpeed);
+
+    follower.lastCall = currentTime;
+}
+
+constrain = function(value, max, min) {
+    if(value < min) return min;
+    if(value > max) return max;
+    return value;
+}
+
+module.exports = {
+    followPath
+};
--- public/js/p5/path_gen.js
+++ public/js/p5/path_gen.js
@@ -1,6 +1,6 @@
 const Waypoint = require("./waypoint");
 
-injectPoints = function (userPoints, injectedPoints, spacing) {
+injectPoints = function(userPoints, injectedPoints, spacing) {
     injectedPoints.splice(0, injectedPoints.length);
 
     for (let i = 0; i < userPoints.length - 1; i++) {
@@ -17,7 +17,7 @@ injectPoints = function (userPoints, injectedPoints, spacing) {
     }
 }
 
-smoothPoints = function (injectedPoints, smoothedPoints, weight) {
+smoothPoints = function(injectedPoints, smoothedPoints, weight) {
     smoothedPoints.splice(0, smoothedPoints.length);
 
     for (waypoint of injectedPoints) {
@@ -54,7 +54,88 @@ smoothPoints = function (injectedPoints, smoothedPoints, weight) {
     }
 }
 
+calculateDistances = function(points, distanceBetween) {
+    for(let i = 0; i < points.length; i++) {
+        if(i == 0) {
+            distanceBetween.push(0.0);
+        }
+        else {
+            let prev = points[i - 1];
+            let curr = points[i];
+
+            distanceBetween.push(curr.getDistanceTO(prev));
+        }
+    }
+}
+
+calculateCurvatures = function(points, curvatures) {
+    for(let i = 0; i < points.length; i++) {
+        if(i == 0 || i == points.length - 1) {
+            curvatures.push(0);
+            continue;
+        }
+
+        let curr = points[i];
+        let prev = points[i - 1];
+        let next = points[i + 1];
+
+        let x1 = curr.getX();
+        let y1 = curr.getY();
+
+        let x2 = prev.getX();
+        let y2 = prev.getY();
+
+        let x3 = next.getX();
+        let y3 = next.getY();
+
+        if(x1 == x2) x1 += 0.0001; // get rid of divide by 0
+
+        let k1 = 0.5 * (x1 * x1 + y1 * y1 - x2 * x2 - y2 * y2) / (x1 - x2);
+        let k2 = (y1 - y2) / (x1 - x2);
+        let b = 0.5 * (x2 * x2 - 2 * x2 * k1 + y2 * y2 - x3 * x3 + 2 * x3 * k1 - y3 * y3) / 
+            (x3 * k2 - y3 + y2 - x2 * k2);
+        let a = k1 - k2 * b;
+        let r = Math.sqrt((x1 - a) * (x1 - a) + (y1 - b) + (y1 - b));
+
+        let curvature;
+        if(r == 0) {
+            curvature = 0.0;
+        }
+        else {
+            curvature = 1.0 / r;
+        }
+
+        curvatures.push(curvature);
+    }
+}
+
+calculateTargetVelocities = function(points, maxVelocity, maxAcceleration, turningConstant) {
+    let distanceBetween = []; // distance between a point and its previous one
+    let curvatures = [];
+
+    calculateDistances(points, distancePath, distanceBetween);
+    calculateCurvatures(points, curvatures);
+
+    for(let i = 0; i < points.length; i++) {
+        points[i].setTargetVelocity(Math.min(maxVelocity, turningConstant / curvatures[i]));
+    }
+
+    for(let i = points.length - 1; i >= 0; i--) {
+        if(i == points.length - 1) {
+            points[i].setTargetVelocity(0);
+            continue;
+        }
+
+        let dist = distanceBetween[i + 1];
+        let nextVelocity = points[i + 1].getTargetVelocity();
+        let calculatedSpeed = Math.sqrt(nextVelocity * nextVelocity + 2.0 * maxAcceleration * dist);
+
+        points[i].setTargetVelocity(Math.min(points[i].getTargetVelocity(), calculatedSpeed));
+    }
+}
+
 module.exports = {
     injectPoints,
-    smoothPoints
+    smoothPoints,
+    calculateTargetVelocities
 }
