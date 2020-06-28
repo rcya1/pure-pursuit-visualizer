e11e1a23236c8ff949108d7f1f9dc30f67c1543e
ryan10145 <chang.ryan10145@gmail.com>
2020-06-28 14:21:58 -0400

    Add Tests for Vector

--- tests/ts/vector.ts
+++ tests/ts/vector.ts
@@ -5,20 +5,16 @@ import 'mocha';
 describe('Vector', () => {
     it('constructor', () => {
         const vector: Vector = new Vector(1, 1);
-        expect(vector).not.equal(null);
+        validateVectorExact(vector, 1, 1);
     });
 
     it('getters and setters for (x, y)', () => {
         const vector: Vector = new Vector(1, 2);
-        expect(vector).not.equal(null);
-        expect(vector.x).equal(1);
-        expect(vector.y).equal(2);
+        validateVectorExact(vector, 1, 2);
         vector.x = 3;
-        expect(vector.x).equal(3);
-        expect(vector.y).equal(2);
+        validateVectorExact(vector, 3, 2);
         vector.y = 4;
-        expect(vector.x).equal(3);
-        expect(vector.y).equal(4);
+        validateVectorExact(vector, 3, 4);
     });
 
     it('magnitude', () => {
@@ -35,18 +31,73 @@ describe('Vector', () => {
 
     it('normalize', () => {
         const vector: Vector = new Vector(1, 2);
-        expect(vector).not.equal(null);
-
         const norm: Vector = vector.normalize();
-        expect(vector).not.equal(null);
-        expect(norm).not.equal(null);
         
-        expect(vector.x).equal(1);
-        expect(vector.y).equal(2);
-
-        expect(norm.x).to.be.closeTo(0.447, 0.001);
-        expect(norm.y).to.be.closeTo(0.894, 0.001);
+        validateVectorExact(vector, 1, 2);
+        validateVectorApprox(norm, 0.447, 0.894, 0.001);
 
         expect(norm.mag).to.be.closeTo(1, 0.0001);
     });
+
+    it('add / sub', () => {
+        const a: Vector = new Vector(1, 2);
+        const b: Vector = new Vector(3, 5);
+
+        const c: Vector = a.add(b);
+        validateVectorExact(a, 1, 2);
+        validateVectorExact(c, 4, 7);
+
+        const d: Vector = b.sub(a);
+        validateVectorExact(a, 1, 2);
+        validateVectorExact(d, 2, 3);
+    });
+
+    it('scalar mult', () => {
+        const vector: Vector = new Vector(2, 4);
+        const mult: number = 3;
+
+        const product: Vector = vector.mult(mult);
+        validateVectorExact(vector, 2, 4);
+        validateVectorExact(product, 6, 12);
+    });
+
+    it('dot', () => {
+        const a: Vector = new Vector(2, 4);
+        const b: Vector = new Vector(3, 5);
+
+        const dot1: number = a.dot(b);
+        const dot2: number = b.dot(a);
+
+        expect(dot1).equal(26);
+        expect(dot2).equal(26);
+    });
+
+    it('get distance', () => {
+        const a: Vector = new Vector(2, 5);
+        const b: Vector = new Vector(3, 20);
+
+        const distSq1: number = a.getDistanceToSq(b);
+        const distSq2: number = b.getDistanceToSq(a);
+
+        expect(distSq1).equal(226);
+        expect(distSq2).equal(226);
+
+        const dist1: Number = a.getDistanceTo(b);
+        const dist2: Number = b.getDistanceTo(a);
+
+        expect(dist1).to.be.closeTo(15.033, 0.001);
+        expect(dist2).to.be.closeTo(15.033, 0.001);
+    });
+
+    function validateVectorExact(vector: Vector, x: number, y: number) {
+        expect(vector).not.equal(null);
+        expect(vector.x).equal(x);
+        expect(vector.y).equal(y);
+    }
+
+    function validateVectorApprox(vector: Vector, x: number, y: number, e: number) {
+        expect(vector).not.equal(null);
+        expect(vector.x).to.be.closeTo(x, e);
+        expect(vector.y).to.be.closeTo(y, e);
+    }
 });
